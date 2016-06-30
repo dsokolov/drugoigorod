bc51cd8ad68df8f61ed76bfb012307a262be1791
Material <So0utt@gmail.com>
2016-07-01 00:11:53 +0400

    First commit, added collapse toolbar, webView & test picture.

--- app/src/main/java/ru/drugoigorod/app/MainActivity.java
+++ app/src/main/java/ru/drugoigorod/app/MainActivity.java
@@ -1,6 +1,29 @@
 package ru.drugoigorod.app;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+            collapsingToolbarLayout.setTitle("Test Collapse");
+            collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(R.color.colorAccent));
+            collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.colorAccent));
+        }
+    }
 }
