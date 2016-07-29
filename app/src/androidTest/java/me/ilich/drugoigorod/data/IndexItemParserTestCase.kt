d1ba35b462a9d5c635ac2a346593f496c0cf789b
dmitry.i.sokolov <DmitryIlichSokolov@gmail.com>
2016-07-29 12:21:47 +0400

    парсинг оглавления

--- app/src/androidTest/java/me/ilich/drugoigorod/data/IndexItemParserTestCase.kt
+++ app/src/androidTest/java/me/ilich/drugoigorod/data/IndexItemParserTestCase.kt
@@ -12,6 +12,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import ru.drugoigorod.app.data.IndexItem
 import ru.drugoigorod.app.data.IndexItemParser
+import ru.drugoigorod.app.data.Style
 
 @RunWith(AndroidJUnit4::class)
 @SmallTest
@@ -30,11 +31,50 @@ class IndexItemParserTestCase {
         val parsed = parser?.parse(AssertsTools.asString(context, "indexes/index.html"))
         assertNotNull(parsed)
         val expected = listOf<IndexItem>(
-                IndexItem("ТЕМА ЛЕТА"),
-                IndexItem("ШЕФ, ТРОГАЙ!")
+                IndexItem(
+                        "http://drugoigorod.ru/go-to-metafest/",
+                        "ТЕМА ЛЕТА",
+                        "http://drugoigorod.ru/wp-content/uploads/2016/07/концерт-11.jpg",
+                        Style.BIG
+                ),
+                IndexItem(
+                        "http://drugoigorod.ru/uber-samara-26-07-16/",
+                        "ШЕФ, ТРОГАЙ!",
+                        "http://drugoigorod.ru/wp-content/uploads/2016/07/tumblr_njbu28if9v1qd479ro1_1280.gif",
+                        Style.NORMAL
+                ),
+                IndexItem(
+                        "http://drugoigorod.ru/metafest_activities/",
+                        "ЧТО ДУШЕ УГОДНО",
+                        "http://drugoigorod.ru/wp-content/uploads/2016/07/pA09wik0ke8.jpg",
+                        Style.NORMAL
+                ),
+                IndexItem(
+                        "http://drugoigorod.ru/tsf2016-online/",
+                        "ВОЗВРАЩЕНИЕ ЦВЕТА",
+                        "http://drugoigorod.ru/wp-content/uploads/2016/06/xgDzirkb35A-728x483.jpg",
+                        Style.NORMAL
+                ),
+                IndexItem(
+                        "http://drugoigorod.ru/allprivileges/",
+                        "ВСЁ МОГУТ КОРОЛИ",
+                        "http://drugoigorod.ru/wp-content/uploads/2016/07/luxury_lifestyle-wallpaper-1152x720-773x483.jpg",
+                        Style.NORMAL
+                ),
+                IndexItem(
+                        "http://drugoigorod.ru/street_music/",
+                        "МАМА АНАРХИЯ",
+                        "http://drugoigorod.ru/wp-content/uploads/2016/07/ivan2.jpg",
+                        Style.NORMAL
+                ),
+                IndexItem(
+                        "http://drugoigorod.ru/halyava-2507-3107/",
+                        "ВХОД СВОБОДНЫЙ",
+                        "http://drugoigorod.ru/wp-content/uploads/2016/07/gimage_842_новый-размер1-675x483.jpg",
+                        Style.NORMAL
+                )
         )
         assertArrayEquals(expected.toTypedArray(), parsed?.toTypedArray())
-
     }
 
 }
--- app/src/main/kotlin/ru/drugoigorod/app/data/IndexItem.kt
+++ app/src/main/kotlin/ru/drugoigorod/app/data/IndexItem.kt
@@ -1,5 +1,12 @@
 package ru.drugoigorod.app.data
 
-data class IndexItem(val title: String) {
+data class IndexItem(
+        val articleId: String,
+        val title: String,
+        val backgroundUrl: String,
+        val style: Style
+)
 
+enum class Style {
+    BIG, MIDDLE, NORMAL, NOPIC
 }
